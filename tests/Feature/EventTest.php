8bdc7775cf9e19a46c92eeb1b5bca153c338fa73
Tom Hayes <tom@theideabureau.co>
2025-06-25 19:18:21 +0100

    add simple data vaildation tests for event creation

--- tests/Feature/EventTest.php
+++ tests/Feature/EventTest.php
@@ -24,3 +24,48 @@ test('an event can be created', function () {
                  ->etc()
         );
 });
+
+test('event creation fails without a title', function () {
+    $payload = [
+        // 'title' => missing on purpose
+        'location' => 'London',
+        'start_date' => '2025-09-01',
+        'end_date' => '2025-09-02',
+    ];
+
+    $response = postJson('/api/events', $payload);
+
+    $response
+        ->assertStatus(422)
+        ->assertJsonValidationErrors(['title']);
+});
+
+test('event creation fails if end_date is before start_date', function () {
+    $payload = [
+        'title' => 'Laravel London',
+        'location' => 'London',
+        'start_date' => '2025-09-02',
+        'end_date' => '2025-09-01', // invalid
+    ];
+
+    $response = postJson('/api/events', $payload);
+
+    $response
+        ->assertStatus(422)
+        ->assertJsonValidationErrors(['end_date']);
+});
+
+test('event creation fails if location is missing', function () {
+    $payload = [
+        'title' => 'Laravel London',
+        // 'location' => missing on purpose
+        'start_date' => '2025-09-01',
+        'end_date' => '2025-09-02',
+    ];
+
+    $response = postJson('/api/events', $payload);
+
+    $response
+        ->assertStatus(422)
+        ->assertJsonValidationErrors(['location']);
+});
