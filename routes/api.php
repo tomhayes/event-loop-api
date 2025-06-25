7ce1ca090d8ca7bfd59882dd8650d51a0b8b52e9
Tom Hayes <tom@theideabureau.co>
2025-06-25 21:33:58 +0100

    add route for get events request

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/events', [EventController::class, 'store']);
+Route::get('/events', [EventController::class, 'index']);
