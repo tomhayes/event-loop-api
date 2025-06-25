e92aaea237139c5abc71ebe5b13242fd004bc4c2
Tom Hayes <tom@theideabureau.co>
2025-06-25 23:07:29 +0100

    add update api route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/events', [EventController::class, 'store']);
 Route::get('/events', [EventController::class, 'index']);
 Route::get('/events/{event}', [EventController::class, 'show']);
+Route::put('/events/{event}', [EventController::class, 'update']);
