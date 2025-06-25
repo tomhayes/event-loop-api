a4b64429ee52e6ff99e7c740350b0536e22d9f66
Tom Hayes <tom@theideabureau.co>
2025-06-25 22:38:04 +0100

    add api route for single event

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/events', [EventController::class, 'store']);
 Route::get('/events', [EventController::class, 'index']);
+Route::get('/events/{event}', [EventController::class, 'show']);
